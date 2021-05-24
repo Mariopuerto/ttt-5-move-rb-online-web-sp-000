35f75d4b0102896651c37e5c87717e663882f7a6
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-05-24 16:44:56 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,13 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(input)
-  input.to_i - 1
+# def input_to_index(input)
+#   input.to_i - 1
 end
-
-def move
-  
-end
+# 
+# def move
+#   
+# end
 # def input_to_index(input)
 #   input.to_i - 1
 # end
